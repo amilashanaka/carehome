9c25fecc5de42e43bd058be11a9d74631a5a15e0
iyonawaduge <iyonawaduge@gmail.com>
2025-01-24 10:48:53 +0530

    (add spase  event table title)

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -18,38 +18,38 @@ overlay.addEventListener('click', () => {
   overlay.classList.remove('active');
 });
 
-function animateText(elementId) {
-  const text = document.getElementById(elementId);
-  const textContent = text.innerHTML;
-  text.innerHTML = ''; // Clear the content of the element
+// function animateText(elementId) {
+//   const text = document.getElementById(elementId);
+//   const textContent = text.innerHTML;
+//   text.innerHTML = ''; // Clear the content of the element
 
-  // Wrap each letter in a span
-  for (let i = 0; i < textContent.length; i++) {
-    var out = textContent[i]; 
-    const span = document.createElement('span');
-    if(textContent[i]=="<"){
-      out = "<br>";
-      i += 4;
-    }
+//   // Wrap each letter in a span
+//   for (let i = 0; i < textContent.length; i++) {
+//     var out = textContent[i]; 
+//     const span = document.createElement('span');
+//     if(textContent[i]=="<"){
+//       out = "<br>";
+//       i += 4;
+//     }
 
-    span.textContent = out;
-    text.appendChild(span);
-    span.style.animationDelay = `${i * 0.1}s`; // Delay each letter
-  }
+//     span.textContent = out;
+//     text.appendChild(span);
+//     span.style.animationDelay = `${i * 0.1}s`; // Delay each letter
+//   }
 
-  // Add the class for animation
-  text.classList.add('fade-in-text');
-}
+//   // Add the class for animation
+//   text.classList.add('fade-in-text');
+// }
 
-// Trigger the animation after each slide change
-document.addEventListener('slide.bs.carousel', function () {
-  animateText('first-slide-title');
-  animateText('first-slide-text');
-});
+// // Trigger the animation after each slide change
+// document.addEventListener('slide.bs.carousel', function () {
+//   animateText('first-slide-title');
+//   animateText('first-slide-text');
+// });
 
-// Initial trigger for the first slide (use setTimeout for initial delay)
-setTimeout(function () {
-  animateText('first-slide-title');
-  animateText('first-slide-text');
-}, 500);  
+// // Initial trigger for the first slide (use setTimeout for initial delay)
+// setTimeout(function () {
+//   animateText('first-slide-title');
+//   animateText('first-slide-text');
+// }, 500);  
 
