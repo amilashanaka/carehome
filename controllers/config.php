92d0369cce56b85565aeb7ca2703cb7cbe9ec306
amilashanaka <dsa.amilashanaka@gmail.com>
2025-02-06 19:13:58 +0000

    update

--- controllers/config.php
+++ controllers/config.php
@@ -2,13 +2,13 @@
 // Db connection
 define('DB_HOST','localhost');
 
-define('DB_USER','root');
-define('DB_PASS', '');
-define('DB_NAME','crhm');
+// define('DB_USER','root');
+// define('DB_PASS', '');
+// define('DB_NAME','crhm');
 
-// define('DB_USER','u710400283_crhm');
-// define('DB_PASS', 'Care@2025#456');
-// define('DB_NAME','u710400283_crhm');
+define('DB_USER','u710400283_crhm');
+define('DB_PASS', 'Care@2025#456');
+define('DB_NAME','u710400283_crhm');
  
 
 // URI parameters
