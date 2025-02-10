a95d25cf7ca688c2199fec6642c11a026d330021
amilashanaka <dsa.amilashanaka@gmail.com>
2025-02-10 15:22:14 +0000

    update

--- footer.php
+++ footer.php
@@ -33,7 +33,7 @@
         <h5>Contact Info</h5>
         <div class="contact-info">
           <p><strong>Address:</strong><br>121 King Street, Melbourne,<br>3000, Australia</p>
-          <p><strong>Email:</strong><br><a href="mailto:info@sencare.com">info@sencare.com</a></p>
+          <p><strong>Email:</strong><br><a href="<?= $setting->getSettings('f5')?>"><?= $setting->getSettings('f5')?></a></p>
           <p><strong>Phone:</strong><br><?= $setting->getSettings('f4')?></p>
         </div>
       </div>
